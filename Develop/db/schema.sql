6026d152522457a7844b7f86b7892513c628a1f7
ebrest993 <elliott.brest@gmail.com>
2023-09-11 06:13:35 -0500

    database created (ecommerce_db)

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,31 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id              INT             NOT NULL    PRIMARY KEY                         ,
+    category_name   VARCHAR(30)     NOT NULL
+);
+
+CREATE TABLE product (
+    id              INT             NOT NULL    PRIMARY KEY     AUTO_INCREMENT      ,
+    product_name    VARCHAR(30)     NOT NULL                                        ,
+    price           DECIMAL         NOT NULL                    '_______________'   ,
+    stock           INT             NOT NULL    10              '_______________'   ,
+    category_id,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+
+CREATE TABLE tag (
+    id              INT             NOT NULL    PRIMARY KEY     AUTO_INCREMENT      ,
+    tag_name        VARCHAR(50)
+);
+
+CREATE TABLE productTag (
+    id              INT             NOT NULL    PRIMARY KEY     AUTO_INCREMENT      ,
+    product_id      INT                                                             ,
+    FOREIGN KEY (product_id) REFERENCES product(id)
+
+);
---
+++ Develop/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (id, category_name)
+VALUE   ('Integer'),
+        ("Doesn't allow null values."),
+        ('Set as primary key');
+
+INSERT INTO 

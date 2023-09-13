b564e7b32460e537dcd41ee4d3915c84e7da8477
ebrest993 <elliott.brest@gmail.com>
2023-09-12 20:52:05 -0500

    tag query finished

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -46,16 +46,13 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
-  try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [
-        { model: Tag }
-      ]
-    })
-  } catch (err) {
-    res.status(500).json(err);
-  }  
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.delete('/:id', async (req, res) => {
